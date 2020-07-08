7127214c5df90cf167846b6409c4c661fe9a2895
suadfatih <67868338+suadfatih@users.noreply.github.com>
2020-07-08 12:27:18 +0200

    Add files via upload

---
+++ E2311 _Suadfatih_ifconditon.py
@@ -0,0 +1,6 @@
+b = "Suadfatih"
+a = input("Please insert your first name : ")
+if a == b:
+    print("Hello {}! The password is : Deutsch ".format(a))
+else :
+    print("Hello {}! See you later.".format(a))
